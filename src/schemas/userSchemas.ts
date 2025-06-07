f57b3c720081eb69ce98d78d54bac6d3ae2f5283
Kevin Souza <kevinwillyansouza@gmail.com>
2025-06-07 17:29:43 -0300

    feat: atualizar schema de validação do usuário

--- src/schemas/userSchemas.ts
+++ src/schemas/userSchemas.ts
@@ -1,18 +1,46 @@
 import { z } from 'zod'
 
+const NAME_MIN_LENGTH = 3
+const PASSWORD_LENGTH = 6
+
 export const createUserSchema = z.object({
-  name: z.string().min(2),
-  email: z.string().email(),
-  password: z.string().min(6)
+  name: z.string().min(NAME_MIN_LENGTH, {
+    message: `Nome deve ter pelo menos ${NAME_MIN_LENGTH} caracteres`
+  }),
+  email: z.string().email({
+    message: 'Email inválido'
+  }),
+  password: z.string().min(PASSWORD_LENGTH, {
+    message: `Senha deve ter pelo menos ${PASSWORD_LENGTH} caracteres`
+  })
 })
 
 export const updateUserSchema = z.object({
-  name: z.string().min(2).optional(),
-  email: z.string().email().optional(),
-  password: z.string().min(6).optional()
+  name: z
+    .string()
+    .min(NAME_MIN_LENGTH, {
+      message: `Nome deve ter pelo menos ${NAME_MIN_LENGTH} caracteres`
+    })
+    .optional(),
+  email: z
+    .string()
+    .email({
+      message: 'Email inválido'
+    })
+    .optional(),
+  password: z
+    .string()
+    .min(PASSWORD_LENGTH, {
+      message: `Senha deve ter pelo menos ${PASSWORD_LENGTH} caracteres`
+    })
+    .optional()
 })
 
 export const loginUserSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(6)
+  email: z.string().email({
+    message: 'Email inválido'
+  }),
+  password: z.string().min(PASSWORD_LENGTH, {
+    message: `Senha deve ter pelo menos ${PASSWORD_LENGTH} caracteres`
+  })
 })
